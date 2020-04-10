c582a481964df92ce27baa06563577d5a9735d76
omfuke <46218908+omfuke@users.noreply.github.com>
2020-04-10 17:31:54 +0530

    'complete'

--- README.md
+++
@@ -1,2 +0,0 @@
-# DigitRecognizeWeb
-web based application for digit recognistion
--- app.py
+++ app.py
@@ -2,37 +2,38 @@ from flask import Flask,request,render_template,redirect,url_for,jsonify
 from keras.models import load_model
 import numpy as np
 import tensorflow as tf
-import cv2
+from flask_cors import CORS
+
+
+
+import json
 
 app = Flask(__name__)
+
+CORS(app)
 model = load_model('model.h5', custom_objects=None,
                    compile=False
                    )
 
+
 graph = tf.get_default_graph()
+@app.route('/')
+def index():
 
+    return render_template('index.html')
 
-@app.route("/",methods=['POST','GET'])
+@app.route("/predict",methods=['POST','GET'])
 def home():
-    if request.method == 'POST':
-        img = request.files['img']
-        response = img.read()
-        img = cv2.imdecode(np.fromstring(response, np.uint8), cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img,(28,28))
-        print(img)
-        my_array = img/255.0
-        my_array = my_array.reshape(-1,28,28,1)
-
-        with graph.as_default():
-            y = model.predict(my_array)
-        predicted_value = int(np.argmax(y, axis=1)[0])
-
-        return jsonify({"predicted": predicted_value})
-
-    else:
-        return render_template('base.html')
-
-
+    my_array = np.array(list(request.form['data']))
+    my_array = my_array
+    my_array = my_array.reshape(-1, 28, 28, 1)
+    with graph.as_default():
+        y = model.predict(my_array)
+    predicted_value = int(np.argmax(y, axis=1)[0])
+    # print(request.args['name'])
+    return jsonify({"predicted": predicted_value})
+    # print(request.args['data'])
+    # return request.form['data']
 
 if __name__ == "__main__":
     app.run(debug=True)
